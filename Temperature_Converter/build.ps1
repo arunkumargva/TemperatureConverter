649a75c5b550c663a9576409141e428e24d6f387
arunkumargva <arunkumar.gva@gmail.com>
2025-03-11 22:24:21 +0000

    test version changes

--- Temperature_Converter/build.ps1
+++ Temperature_Converter/build.ps1
@@ -1,3 +1,4 @@
-# Use dotnet to build the WPF project file
+# build.ps1
+# Your build commands go here
+Set-Location -Path $PSScriptRoot
 dotnet build "Temperature_Converter.csproj" -c Release -f net8.0-windows -o ./output
- 
